6fe10fb564489e682e65e7f9e224421383cdf810
amir811537 <amirhossain.bc.75@gmail.com>
2023-12-25 14:38:50 +0600

    all api work

--- index.js
+++ index.js
@@ -7,16 +7,10 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port= process.env.PORT || 5000
 
 
+// Midlewares
+app.use(cors())
+app.use(express.json())
 
-// midewaare
-app.use(cors({
-  origin: [
-      'http://localhost:5173',
-  ],
-  credentials: true
-}));
-app.use(express.json());
-app.use(cookieParser());
 
 
 // const port = process.env.PORT || 5000;
